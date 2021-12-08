10b245dfad8ba8a2c9a424dc71668d6b1fa6f95b
franc <franc@HAKI.home>
2021-12-08 17:37:32 +0100

    Aggiornamento classe User.java Aggiunti attributi base della classe

--- src/main/java/servlet/User.java
+++ src/main/java/servlet/User.java
@@ -11,4 +11,13 @@ package servlet;
  */
 public abstract class User {
     
+    protected String name;
+    protected String surname;
+    protected String email;         //Inserire campo di conferma nel modulo, caso doppia registrazione
+    protected String birthDate;
+    protected String phoneNumber;   //Opzionale
+    protected String userId;
+    protected String password;      //Inserire campo di conferma nel modulo. Nel caso doppia registrazione le password devono essere diverse
+    
+    
 }
