366178122c271a0af97a48529bbf190a75dbcd06
Silvio Zambon <silvio.zambon@studenti.unitn.it>
2021-12-08 22:48:07 +0100

    Aggiunta classe servlet e aggiunto driver per mondodb del pom.xml

---
+++ src/main/java/servlet/ServletHF.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author Silvio
+ */
+@WebServlet(name = "ServletHF", urlPatterns = "/app/*")
+public class ServletHF extends HttpServlet{
+    
+}
