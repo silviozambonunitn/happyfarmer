9477cb3fb380fa54a609f86c254592693746f3f8
francescounitn <francesco.dallagata@studenti.unitn.it>
2021-12-08 18:31:50 +0100

    Aggiornamento classe User.java Aggiunti ultimi setter, commentato Prodotto.image

--- src/main/java/servlet/User.java
+++ src/main/java/servlet/User.java
@@ -7,7 +7,7 @@ package servlet;
 
 /**
  *
- * @author Silvio
+ * @author silviozambonunitn, francescounitn
  */
 public abstract class User {
     
@@ -23,21 +23,57 @@ public abstract class User {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getSurname() {
         return surname;
     }
 
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+
     public String getBirthDate() {
         return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public String getUserId() {
         return userId;
     }
-    
-    
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+        
 }
