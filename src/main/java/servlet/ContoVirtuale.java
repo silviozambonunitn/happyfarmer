1c0d286357324d35143ea88cb8aa7dda567ac85d
Silvio Zambon <silvio.zambon@studenti.unitn.it>
2021-12-22 10:03:31 +0100

    Ristrutturata classe per permettere implementazione e salvataggio in db

--- src/main/java/servlet/ContoVirtuale.java
+++ src/main/java/servlet/ContoVirtuale.java
@@ -1,21 +1,48 @@
 package servlet;
 
 import java.util.ArrayList;
+import org.bson.codecs.pojo.annotations.BsonCreator;
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.types.ObjectId;
 
 /**
  *
  * @author Silvio
  */
 public class ContoVirtuale {
-    private int id;//o string
+    @BsonId
+    private String id;
     private double value;
     private ArrayList<Pagamento> pagamenti;
-    
-    public boolean updateSaldo(){
-        return false;
+
+    @BsonCreator
+    public ContoVirtuale() {
+        id=new ObjectId().toHexString();
+        value=0;
+        pagamenti=new ArrayList<>();
     }
-    
-    public boolean bonifico(){
-        return false;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public ArrayList<Pagamento> getPagamenti() {
+        return pagamenti;
+    }
+
+    public void setPagamenti(ArrayList<Pagamento> pagamenti) {
+        this.pagamenti = pagamenti;
     }
 }
